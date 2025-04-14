f8281a68a0bd37a583b04b7b8325c7db79086b48
DenDenDor <dendor333@gmail.com>
2025-04-14 18:56:58 +0300

    Add new bullet

--- GUNS_Project/Assets/Scripts/Battle/Router/BattleRouter.cs
+++ GUNS_Project/Assets/Scripts/Battle/Router/BattleRouter.cs
@@ -35,7 +35,7 @@ public class BattleRouter : IRouter
 
     private void OnRemoved()
     {
-        if (EntityController.Instance.FullEntities.Keys.Count(x=>x is SoldierView) == 0)
+        if (EntityController.Instance.FullEntities.Keys.Count(x=>x is SoldierView) == 0 || EntityController.Instance.Enemies.Count == 0)
         {
             BattleController.Instance.Restart();
             OnRestarted();
